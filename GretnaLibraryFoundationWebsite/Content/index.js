0311b9453271f8a37734b32345736cba846e054d
saingledue <saingledue@outlook.com>
2018-05-31 16:51:56 -0500

    got basic layout done

---
+++ GretnaLibraryFoundationWebsite/Content/index.js
@@ -0,0 +1,47 @@
+//window.onload = () => {
+//    let path = window.location.pathname;
+//    let page = path.split("/").pop();
+//    switch (page) {
+//        case "bhIndex.html":
+//            break;
+//        case "bhPurchaseItems.html":
+//            createBountyHunter();
+//            break;
+//        case "bhEquipGear.html":
+//            loadWeapons();
+//            break;
+//        case "rodianYes1.html":
+//            rehalRodian = new opponent("Rehal", "Madar", 10, 5, 10);
+//            getBountyHunterData();
+//            document.getElementById("firstName").innerHTML = rehalRodian["firstName"].toString();
+//            document.getElementById("lastName").innerHTML = rehalRodian["lastName"].toString();
+//            document.getElementById("combatOffense").innerHTML = rehalRodian["combatOffense"].toString();
+//            document.getElementById("combatDefense").innerHTML = rehalRodian["combatDefense"].toString();
+//            document.getElementById("health").innerHTML = rehalRodian["health"].toString();
+//            break;
+//        default:
+//            break;
+//    }
+//};
+var GoToHome = function () {
+    window.location.href = "index.html";
+};
+var GoToAboutCampaign = function () {
+    window.location.href = "aboutcampaign.html";
+};
+var GoToMission = function () {
+    window.location.href = "mission.html";
+};
+var GoToVision = function () {
+    window.location.href = "vision.html";
+};
+var GoToContact = function () {
+    window.location.href = "contact.html";
+};
+var GoToHistory = function () {
+    window.location.href = "history.html";
+};
+var GoToProof = function () {
+    window.location.href = "proof.html";
+};
+//# sourceMappingURL=index.js.map
---
+++ GretnaLibraryFoundationWebsite/Content/index.ts
@@ -0,0 +1,49 @@
+﻿//window.onload = () => {
+//    let path = window.location.pathname;
+//    let page = path.split("/").pop();
+//    switch (page) {
+//        case "bhIndex.html":
+//            break;
+//        case "bhPurchaseItems.html":
+//            createBountyHunter();
+//            break;
+//        case "bhEquipGear.html":
+//            loadWeapons();
+//            break;
+//        case "rodianYes1.html":
+//            rehalRodian = new opponent("Rehal", "Madar", 10, 5, 10);
+
+//            getBountyHunterData();
+
+//            document.getElementById("firstName").innerHTML = rehalRodian["firstName"].toString();
+//            document.getElementById("lastName").innerHTML = rehalRodian["lastName"].toString();
+//            document.getElementById("combatOffense").innerHTML = rehalRodian["combatOffense"].toString();
+//            document.getElementById("combatDefense").innerHTML = rehalRodian["combatDefense"].toString();
+//            document.getElementById("health").innerHTML = rehalRodian["health"].toString();
+
+//            break;
+//        default:
+//            break;
+//    }
+//};
+let GoToHome = () => {
+    window.location.href = "index.html";
+}
+let GoToAboutCampaign = () => {
+    window.location.href = "aboutcampaign.html";
+}
+let GoToMission = () => {
+    window.location.href = "mission.html";
+}
+let GoToVision = () => {
+    window.location.href = "vision.html";
+}
+let GoToContact = () => {
+    window.location.href = "contact.html";
+}
+let GoToHistory = () => {
+    window.location.href = "history.html";
+}
+let GoToProof = () => {
+    window.location.href = "proof.html";
+}
